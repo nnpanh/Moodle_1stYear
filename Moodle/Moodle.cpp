798df0137eb1d897beac52b584b5e068b05694ce
Duc Hieu <tranthaiduchieu@gmail.com>
2020-04-17 22:33:20 +0700

    17/04

--- Moodle/Moodle.cpp
+++ Moodle/Moodle.cpp
@@ -2,6 +2,22 @@
 
 int main() {
 	ifstream fin("19CLC5.csv");
+<<<<<<< HEAD
+	Student s;
+
+	fin.ignore(500, '\n');
+	/*while(!fin.eof()){
+
+		loadStudent(fin, s);
+		printStudent(s);
+	}
+	fin.close();*/
+
+	Class CLC[10];
+	loadClass(fin, CLC, s);
+}
+
+=======
 
 	Student* CLC5 = new Student[50],
 		* CLC6 = new Student[5];
@@ -21,3 +37,4 @@ int main() {
 	delete[] CLC6;
 	delete[] CLC5;
 }
+>>>>>>> 48011cfceffef52a0a8f16a6ea01efd1c8de542b
--- Moodle/MoodleHeader.h
+++ Moodle/MoodleHeader.h
@@ -16,15 +16,28 @@ struct Student {
 	bool Gender;
 	DoB Bday;
 	string ID, LastName, FirstName;
+<<<<<<< HEAD
+	Student* nextStudent;
 };
 
 struct Class {
+	int Size; 
+=======
+};
+
+struct Class {
+>>>>>>> 48011cfceffef52a0a8f16a6ea01efd1c8de542b
 	Student* pStudent;
 };
 
 void printStudent(Student s);
+<<<<<<< HEAD
+void loadStudent(ifstream& f, Student*& s);
+=======
 void loadStudent(ifstream& f, Student& s);
 void saveClass(ofstream& f, Student* pStudent);
+>>>>>>> 48011cfceffef52a0a8f16a6ea01efd1c8de542b
 
+void loadClass(ifstream& f, Class CLC[], Student& s);
 #endif // !_MOODLE_H_
 
